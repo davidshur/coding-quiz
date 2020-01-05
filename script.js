87ce51e709229c74e391d58f3cd1a113287cfb52
David <david@thejurne.com>
2020-01-05 13:28:29 -0800

    refactor code to make a question function

--- script.js
+++ script.js
@@ -5,8 +5,22 @@ $(document).ready(() => {
   const confirmation = $('#confirmation');
   const startButton = $('#start-button');
 
+  let currentQuestion = 0
+
+  const writeQuestion = () => {
+    title.text(questions[currentQuestion].title);
+    content.empty();
+    for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
+      let choice = $('<button>');
+      choice.addClass('btn btn-primary btn-block choice-button');
+      choice.attr('data-choice', questions[currentQuestion].choices[i]);
+      choice.text(questions[currentQuestion].choices[i])
+      content.append(choice);
+    }
+  }
+
   $('#start-button').on('click', () => {
-    let currentQuestion = 0
+
 
     // set quiz timer
     timeLeft = questions.length * 15;
@@ -18,21 +32,19 @@ $(document).ready(() => {
 
     // populate page content
     title.text(questions[currentQuestion].title);
-    for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
-      let choice = $('<button>');
-      choice.addClass('btn btn-primary btn-block choice-button');
-      choice.attr('data-choice', questions[currentQuestion].choices[i]);
-      choice.text(questions[currentQuestion].choices[i])
-      content.append(choice);
-    }
+    writeQuestion();
 
     $('.choice-button').on('click', event => {
       let userChoice = $(event.target).data('choice');
       let answer = questions[currentQuestion].answer;
 
       if (userChoice === answer) {
+        currentQuestion++;
+        writeQuestion();
         confirmation.text('Correct!');
       } else {
+        currentQuestion++;
+        writeQuestion();
         confirmation.text('Incorrect...');
       }
     });
