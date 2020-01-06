7cd92d820454ffd140a289153982c8cbda10a59a
David <david@thejurne.com>
2020-01-06 14:57:48 -0800

    refactor code

--- script.js
+++ script.js
@@ -1,50 +1,47 @@
 $(document).ready(() => {
+
+  const choices = $('#choices');
+  const confirmation = $('#confirmation');
+  const content = $('.card-text');
+  const startButton = $('#start');
   const timer = $('#timer');
   const title = $('.card-title');
-  const content = $('.card-text');
-  const confirmation = $('#confirmation');
-  const startButton = $('#start-button');
-
-  let currentQuestion = 0
-
-  const writeQuestion = () => {
-    title.text(questions[currentQuestion].title);
-    content.empty();
-    for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
-      let choice = $('<button>');
-      choice.addClass('btn btn-primary btn-block choice-button');
-      choice.attr('data-choice', questions[currentQuestion].choices[i]);
-      choice.text(questions[currentQuestion].choices[i])
-      content.append(choice);
-    }
-  }
-
-  $('#start-button').on('click', () => {
 
 
-    // set quiz timer
+
+  startButton.on('click', () => {
+    let currentQuestion = 0
+
+    startButton.remove();
+    content.remove();
+
     timeLeft = questions.length * 15;
     timer.text('Time: ' + timeLeft);
 
-    // remove page content
-    startButton.remove();
-    content.text('');
+    const writeQuestion = () => {
+      title.text(questions[currentQuestion].title);
+      choices.empty();
+      for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
+        const choice = $('<button>');
+        choice.addClass('btn btn-primary btn-block choice-button');
+        choice.attr('data-choice', questions[currentQuestion].choices[i]);
+        choice.text(questions[currentQuestion].choices[i])
+        choices.append(choice);
+      }
+    }
 
-    // populate page content
-    title.text(questions[currentQuestion].title);
     writeQuestion();
 
     $('.choice-button').on('click', event => {
-      let userChoice = $(event.target).data('choice');
-      let answer = questions[currentQuestion].answer;
+      const userChoice = $(event.target).data('choice');
+      const answer = questions[currentQuestion].answer;
+
+      currentQuestion++;
+      writeQuestion();
 
       if (userChoice === answer) {
-        currentQuestion++;
-        writeQuestion();
         confirmation.text('Correct!');
       } else {
-        currentQuestion++;
-        writeQuestion();
         confirmation.text('Incorrect...');
       }
     });
