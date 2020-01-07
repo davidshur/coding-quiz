309fb6334f17c2d0e5a3df5462ea6a87a05fb9fd
David <david@thejurne.com>
2020-01-06 17:57:28 -0800

    finish main game

--- script.js
+++ script.js
@@ -6,71 +6,109 @@ $(document).ready(() => {
   const startButton = $('#start');
   const timer = $('#timer');
 
-  let currentQuestion = 0;
-  let quizComplete = false;
-  let userScore = 0;
-
-  const clearStartPage = () => {
-    startButton.remove();
-    content.remove();
-  }
-
-  const runTimer = () => {
+  startButton.on('click', () => {
+    let currentQuestion = 0;
+    let userScore = 0;
     timeLeft = questions.length * 15;
     timer.text('Timer: ' + timeLeft);
-    const timerInterval = setInterval(() => {
-      if (userScore !== 0 || timeLeft <= 0) {
-        clearInterval(timerInterval);
-        makeScorePage();
-      } else {
-        timeLeft--;
-        timer.text('Timer: ' + timeLeft);
-      }
-    }, 1000);
-  }
 
-  const makeScorePage = () => {
-    userScore = timeLeft;
-    alert('you scored' + userScore);
-  }
+    const runTimer = setInterval(() => {
+        if (userScore !== 0 || timeLeft <= 0) {
+          clearInterval(runTimer);
+          timer.text('Timer: 0');
+          makeScorePage();
+        } else {
+          timeLeft--;
+          timer.text('Timer: ' + timeLeft);
+        }
+      }, 1000);
 
-  const checkAnswer = () => {
-    $('.choice-button').on('click', event => {
-      const userChoice = $(event.target).data('choice');
-      const answer = questions[currentQuestion].answer;
-
-      if (userChoice === answer) {
-        confirmation.text('Correct!');
-      } else {
-        confirmation.text('Incorrect...');
-        timeLeft -= 15;
-      }
-
-      if (currentQuestion >= questions.length - 1) {
-        userScore = timeLeft;
-      } else {
-        currentQuestion++;
-        fillQuestion();
-      }
-    });
-  }
-
-  const fillQuestion = () => {
-    choices.empty();
-    cardTitle.text(questions[currentQuestion].title);
-    for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
-      const choice = $('<button>');
-      choice.addClass('btn btn-primary btn-block choice-button');
-      choice.attr('data-choice', questions[currentQuestion].choices[i]);
-      choice.text(questions[currentQuestion].choices[i]);
-      choices.append(choice);
+    const clearStartPage = () => {
+      startButton.remove();
+      content.text('');
     }
-    checkAnswer();
-  }
 
-  startButton.on('click', () => {
+    const fillQuestion = () => {
+      choices.empty();
+      cardTitle.text(questions[currentQuestion].title);
+      for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
+        const choice = $('<button>');
+        choice.addClass('btn btn-primary btn-block choice-button');
+        choice.attr('data-choice', questions[currentQuestion].choices[i]);
+        choice.text(questions[currentQuestion].choices[i]);
+        choices.append(choice);
+      }
+
+      checkAnswer();
+    }
+
+    const checkAnswer = () => {
+      $('.choice-button').on('click', event => {
+        const userChoice = $(event.target).data('choice');
+        const answer = questions[currentQuestion].answer;
+
+        if (userChoice === answer) {
+          confirmation.text('Correct!');
+        } else {
+          timeLeft -= 15;
+          confirmation.text('Incorrect...');
+        }
+
+        setTimeout(() => {
+          confirmation.empty();
+        }, 1000);
+
+        if (currentQuestion >= questions.length - 1) {
+          if (timeLeft <= 0) {
+            userScore = 0;
+          } else {
+            userScore = timeLeft;
+          }
+        } else {
+          currentQuestion++;
+          fillQuestion();
+        }
+      });
+    }
+
+    const makeScorePage = () => {
+      const scoreForm = $('<form>');
+      const formGroup = $('<div>');
+      const inputLabel = $('<label>');
+      const userInput = $('<input>');
+      const submitButton = $('<button>');
+
+      choices.empty();
+      cardTitle.text('The quiz is over!');
+      content.text('You scored ' + userScore + ' points! I knew you had it in ya.');
+
+      inputLabel.text('Enter your initials: ');
+      inputLabel.attr('for', 'user-initials');
+
+      userInput.addClass('form-control');
+      userInput.attr('id', 'user-initials');
+
+      submitButton.text('Submit');
+      submitButton.addClass('btn btn-primary');
+      submitButton.attr('type', 'submit');
+      submitButton.attr('id', 'submit');
+
+      formGroup.addClass('form-group');
+      formGroup.append(inputLabel, userInput);
+
+      scoreForm.append(formGroup, submitButton);
+      content.append(scoreForm);
+
+      $('#submit').on('click', event => {
+        event.preventDefault();
+        const initials = $('#user-initials').val();
+        localStorage.setItem(initials, userScore);
+        console.log(localStorage);
+      });
+    }
+
+    runTimer;
     clearStartPage();
     fillQuestion();
-    runTimer();
   });
 });
