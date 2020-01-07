e262f0f8a9742cf4b94bc1280706d2b3df54afaf
David <david@thejurne.com>
2020-01-06 16:12:24 -0800

    added interval clear and final page

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
 $(document).ready(() => {
+  const cardTitle = $('.card-title');
   const choices = $('#choices');
   const confirmation = $('#confirmation');
   const content = $('.card-text');
   const startButton = $('#start');
   const timer = $('#timer');
-  const title = $('.card-title');
 
-  let currentQuestion = 0
-
-  const fillQuestion = () => {
-    title.text(questions[currentQuestion].title);
-    choices.empty();
-    for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
-      const choice = $('<button>');
-
-      choice.addClass('btn btn-primary btn-block choice-button');
-      choice.attr('data-choice', questions[currentQuestion].choices[i]);
-      choice.text(questions[currentQuestion].choices[i]);
-
-      choices.append(choice);
-    }
-  }
+  let currentQuestion = 0;
+  let quizComplete = false;
+  let userScore = 0;
 
   const clearStartPage = () => {
     startButton.remove();
@@ -30,24 +18,26 @@ $(document).ready(() => {
   const runTimer = () => {
     timeLeft = questions.length * 15;
     timer.text('Timer: ' + timeLeft);
-    setInterval(() => {
-      timeLeft--;
-      timer.text('Timer: ' + timeLeft);
+    const timerInterval = setInterval(() => {
+      if (userScore !== 0 || timeLeft <= 0) {
+        clearInterval(timerInterval);
+        makeScorePage();
+      } else {
+        timeLeft--;
+        timer.text('Timer: ' + timeLeft);
+      }
     }, 1000);
   }
 
-  startButton.on('click', () => {
+  const makeScorePage = () => {
+    userScore = timeLeft;
+    alert('you scored' + userScore);
+  }
 
-    clearStartPage();
-    fillQuestion();
-    runTimer();
-
+  const checkAnswer = () => {
     $('.choice-button').on('click', event => {
       const userChoice = $(event.target).data('choice');
       const answer = questions[currentQuestion].answer;
-
-      currentQuestion++;
-      fillQuestion();
 
       if (userChoice === answer) {
         confirmation.text('Correct!');
@@ -55,6 +45,32 @@ $(document).ready(() => {
         confirmation.text('Incorrect...');
         timeLeft -= 15;
       }
+
+      if (currentQuestion >= questions.length - 1) {
+        userScore = timeLeft;
+      } else {
+        currentQuestion++;
+        fillQuestion();
+      }
     });
+  }
+
+  const fillQuestion = () => {
+    choices.empty();
+    cardTitle.text(questions[currentQuestion].title);
+    for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
+      const choice = $('<button>');
+      choice.addClass('btn btn-primary btn-block choice-button');
+      choice.attr('data-choice', questions[currentQuestion].choices[i]);
+      choice.text(questions[currentQuestion].choices[i]);
+      choices.append(choice);
+    }
+    checkAnswer();
+  }
+
+  startButton.on('click', () => {
+    clearStartPage();
+    fillQuestion();
+    runTimer();
   });
 });
