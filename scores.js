4086f7b7c7c68fccc0b67e1c07a17c465ef3af3d
David <david@thejurne.com>
2020-01-06 18:18:09 -0800

    highscores work

---
+++ scores.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+  const scoreList = $('#score-list');
+  const highscoreList = Object.keys(localStorage);
+
+  for (let i = 0; i < highscoreList.length; i++) {
+    const userItem = $('<li>');
+    userItem.text(highscoreList[i] + ' | ' + localStorage.getItem(highscoreList[i]));
+    scoreList.append(userItem);
+  }
+});
